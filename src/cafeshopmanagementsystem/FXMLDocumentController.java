69db4e7c52e392884a9461b453a6dec025b9cde6
mracy <e.vibe3@gmail.com>
2024-03-20 15:32:14 +0545

    Your commit message here

--- src/cafeshopmanagementsystem/FXMLDocumentController.java
+++ src/cafeshopmanagementsystem/FXMLDocumentController.java
@@ -1,31 +1,100 @@
-
 package cafeshopmanagementsystem;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
-
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
+    private Hyperlink si_forgotPass;
+
     @FXML
-    private Button button;
-    
+    private Button si_loginBtn;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private AnchorPane si_loginForm;
+
+    @FXML
+    private TextField si_username;
+
+    @FXML
+    private Button side_CreateBtn;
+
+    @FXML
+    private AnchorPane side_form;
+
+    @FXML
+    private TextField su_answer;
+
+    @FXML
+    private PasswordField su_password;
+
+    @FXML
+    private ComboBox<?> su_question;
+
+    @FXML
+    private Button su_signupBtn;
+
+    @FXML
+    private AnchorPane su_signupForm;
+
+    @FXML
+    private TextField su_username;
+
+    @FXML
+    private Button side_alreadyHave;
+
+    public void switchForm(ActionEvent event) {
+
+        TranslateTransition slider = new TranslateTransition();
+
+        if (event.getSource() == side_CreateBtn) {
+
+            slider.setNode(side_form);
+            slider.setToX(300);
+            slider.setDuration(Duration.seconds(.5));
+
+            slider.setOnFinished((ActionEvent e) -> {
+                side_alreadyHave.setVisible(true);
+                side_CreateBtn.setVisible(false);
+
+            });
+
+            slider.play();
+
+        } else if (event.getSource() == side_alreadyHave) {
+            
+            slider.setNode(side_form);
+            slider.setToX(0);
+            slider.setDuration(Duration.seconds(.5));
+
+            slider.setOnFinished((ActionEvent e) -> {
+                side_alreadyHave.setVisible(false);
+                side_CreateBtn.setVisible(true);
+
+            });
+
+            slider.play();
+
+        }
+
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
